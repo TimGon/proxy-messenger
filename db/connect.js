d270ab610ac45568734e6d94215914d4cfc6f888
Tim <timgon032@mail.ru>
2026-06-25 22:15:11 +0400

    Connect to db

---
+++ db/connect.js
@@ -0,0 +1,18 @@
+const { Pool} = require('pg')
+require('.env')
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    max: 20,
+    idleTimeoutMillis: 60000, // Закрытие соединение после 60 секунд простоя
+    connectionTimeoutMillis: 2000 // 2 секунды при ожидании соединения
+})
+
+pool.on('connect', () => console.log('Подключен к PostgreSQL'))
+pool.on('error', (err) => console.error('Ошибка при подключении', err))
+
+module.exports = pool
