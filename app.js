8b25eee16b5afd49eef15e03f427f3384b636062
Tim <timgon032@mail.ru>
2026-06-29 16:58:06 +0400

    added handlers in general file http-servers

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const http = require('http')
-require(".env");
+const http = require('http');
+const { webSocketServer } = require('./websocket_service/webSocketHandle');
+const { createSoapServer } = require('./soap_service/soapHandle');
+require("dotenv").config();
 
-const port = process.env.Port || 8080;
+const port = process.env.PORT || 8080;
 
 // Создание HTTP-сервера для обработки SOAP и WebSocket.
 const server = http.createServer((req, res) => {
-  res.writeHead(404, {'Content-Type': 'text/plain'})
+  res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
   res.end('Не найдено')
 })
+// запуск обработчика Soap
+createSoapServer(server)
+
+// запуск обработчика WebSocket
+webSocketServer(server)
 
 server.listen(port, () =>{
   console.log("Прокси-сервер запущен по http://localhost:8080")
