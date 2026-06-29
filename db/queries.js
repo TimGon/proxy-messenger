0c56d782fdd46ed7633ca02190c853eabba00a83
Tim <timgon032@mail.ru>
2026-06-29 17:01:40 +0400

    added waiting in some functions

--- db/queries.js
+++ db/queries.js
@@ -16,14 +16,14 @@ const createUser = async (login, password) => {
     const hashed = await hashPass(password)
     // запрос с параметризации и возвращения айди и логина для ответа на postman
     const query = `
-    INSERT INTO users(login, password_hash) VALUES ($1, $2) RETURNING user_id, login
+    INSERT INTO users(username, password_hash) VALUES ($1, $2) RETURNING user_id, username
     `
-    const res = pool.query(query, [login, hashed])
+    const res = await pool.query(query, [login, hashed])
     return res.rows[0]
 }
 
 const findLoginByUser = async (login) => {
-    const query = `SELECT id, login, password_hash FROM users WHERE login=$1`
+    const query = `SELECT user_id, username, password_hash FROM users WHERE username=$1`
     const res = await pool.query(query, [login])
     return res.rows[0] || null
 }
@@ -38,14 +38,15 @@ const createSession = async (userId) => {
 const validateSession = async (sessionToken) => {
     const query=`SELECT user_id FROM sessions WHERE session_token=$1`
     const res = await pool.query(query, [sessionToken])
-    return res.row[0] ? res.row[0].userId : null
+    console.log("Проверка на получение:", res.rows[0])
+    return res.rows[0] ? res.rows[0].user_id : null
 }
 
-const saveMessage = async (semderId, recieverId, message, sendAt) => {
+const saveMessage = async (senderId, receiverId, message) => {
     const query = `
-    INSERT INTO messages (from_user_id, to_user_id, text, sent_at) VALUES($1,$2,$3,$4) 
-    RETURNING message_id, from_user_id, to_user_id, text, sent_at`
-    const res = await pool.query(query, [semderId, recieverId, message, sendAt])
+    INSERT INTO messages (from_user_id, to_user_id, text, sent_at) VALUES($1,$2,$3, NOW()) 
+    RETURNING message_id, from_user_id, to_user_id, text, sent_at `
+    const res = await pool.query(query, [senderId, receiverId, message])
     return res.rows[0]
 }
 
